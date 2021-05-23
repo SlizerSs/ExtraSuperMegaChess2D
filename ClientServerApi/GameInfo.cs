4c129a75d5e003570fd0916adf15534f61ec02e2
SlizerS <zerelaw@bk.ru>
2021-05-23 15:52:54 +0300

    небольшие изменения в структуре

---
+++ ClientServerApi/GameInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessClient
+{
+    public struct GameInfo
+    {
+        public int GameID { get; set; }
+        public string FEN { get; set; }
+        public string Status { get; set; }
+        public string White { get; set; }
+        public string Black { get; set; }
+        public string LastMove { get; set; }
+        public string YourColor { get; set; }
+        public string Winner { get; set; }
+
+        public GameInfo(NameValueCollection list)
+        {
+            
+            GameID = int.Parse(list["GameID"]);
+            FEN = list["FEN"];
+            Status = list["Status"];
+            White = list["White"];
+            Black = list["Black"];
+            LastMove = list["LastMove"];
+            YourColor = list["YourColor"];
+            Winner = list["Winner"];
+        }
+    }
+}
---
+++ ClientServerApi/PlayerInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessClient
+{
+    public struct PlayerInfo
+    {
+        public int PlayerID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Games { get; set; }
+        public int Wins { get; set; }
+        public int Loses { get; set; }
+
+
+        public PlayerInfo(NameValueCollection list)
+        {
+            PlayerID = int.Parse(list["PlayerID"]);
+            Name = list["Name"];
+            Password = list["Password"];
+            Games = int.Parse(list["Games"]);
+            Wins = int.Parse(list["Wins"]);
+            Loses = int.Parse(list["Loses"]);
+        }
+    }
+}
